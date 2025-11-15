b785ea2a065234ae8e8d7605da08a1a63b69b342
Arshia <arshiashirvani.f1385@gmail.com>
2025-11-15 23:25:07 +0330

    write views for website app feature you can send your own message in template with all options

--- website/views.py
+++ website/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+import json
 
-# Create your views here.
+def index(request):
+    return render(request, 'notifications.html')
+
+def send_notification(request):
+    if request.method == "POST":
+        title = request.POST.get("title", "پیام")
+        text = request.POST.get("message", "")
+        footer = request.POST.get("footer", "")
+        msg_type = request.POST.get("message_type", "info")
+
+        # پیام را در قالب دیکشنری JSON می‌سازیم
+        msg_content = json.dumps({
+            "title": title,
+            "text": text,
+            "footer": footer,
+            "type": msg_type
+        })
+
+        messages.add_message(request, messages.INFO, msg_content)
+
+    return redirect('website:index')
